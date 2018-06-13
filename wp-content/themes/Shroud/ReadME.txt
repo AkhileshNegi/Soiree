feb649f872907baaade4a62a9c23cff650252793
AkhileshNegi <akhileshnegi.an3@gmail.com>
2018-06-13 15:40:18 +0530

    second commit

---
+++ wp-content/themes/Shroud/ReadME.txt
@@ -0,0 +1,2 @@
+1. Registration.php - to register new guest for soiree
+2. Funciton.php- to create new post type
--- wp-content/themes/Shroud/thankyou.php
+++ wp-content/themes/Shroud/thankyou.php
@@ -6,7 +6,19 @@
 <body>
 	<?php
 
-echo "hello";
+$name= $_POST["name"]; 
+$password= $_POST["password"]; 
+$mail= $_POST["email"];
+$number= $_POST["phone"];
+$city= $_POST["city"];
+$gender= $_POST["gender"];
+
+/*echo "$name";
+echo "$password";
+echo "$mail";
+echo "$number";
+echo "$city";
+echo "$gender";*/
 	?>
 </body>
 </html>
